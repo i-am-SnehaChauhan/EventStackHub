f3aae60f2e32dff41ef89cc38620e906cce2f217
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2024-01-31 13:18:35 +0000

    MobileNav added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,35 +8,36 @@ import MobileNav from "./MobileNav"
 const Header = () => {
   return (
     <header className="w-full border-b">
-        <div className="wrapper flex items-center justify-between">
-           <Link href="/" className="w-36">
-              <Image 
-                src="/assets/images/EventStackHub.png" width={178} height = {58}
-                alt="EventStackHub logo"
-              />
-           </Link>
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/EventStackHub.png" width={178} height={58}
+            alt="EventStackHub logo"
+          />
+        </Link>
 
-           <SignedIn>
-            <nav className="md:flex-between hidden w-full max-w-xs">
-              <MobileNav/>
-            </nav>
-           </SignedIn>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
 
-           <div className="flex w-22 justify-end gap-3">
-              <SignedIn>
-                <UserButton afterSignOutUrl="/"/>
-                <NavItems />
-              </SignedIn>
-              <SignedOut>
-                <Button asChild className="rounded-full bg-blue-500 pr-3 pl-3" size="lg">
-                  <Link href="/sign-in">
-                    Login
-                  </Link>
-                </Button>
-              </SignedOut>
+          </nav>
+        </SignedIn>
 
-           </div>
+        <div className="flex w-22 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full bg-blue-500 pr-3 pl-3" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+
         </div>
+      </div>
     </header>
   )
 }
